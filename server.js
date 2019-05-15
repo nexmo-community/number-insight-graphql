dbb69b014675951c7b5acc05455706590afd1ebb
Martyn Davies <martynrdavies@gmail.com>
2019-05-15 16:25:24 +0200

    initial commit of number insight graphql wrapper

---
+++ server.js
@@ -0,0 +1,92 @@
+const { ApolloServer, gql } = require('apollo-server');
+const NumberInsightAPI = require('./src/number-insight');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Insight {
+    status: Int!
+    status_message: String!
+    request_id: String!
+    international_format_number: String!
+    country_code: String!
+    country_code_iso3: String!
+    country_name: String!
+    country_prefix: String!
+    request_price: String
+    refund_price: String
+    remaining_balance: String
+    ported: String
+    current_carrier: Carrier
+    original_carrier: OriginalCarrier
+    roaming: Roaming
+    lookup_outcome: String
+    lookup_outcome_message: String
+    valid_number: String
+    reachable: String
+    caller_identity: Caller
+    ip: Ip
+    ip_warnings: String
+  }
+
+  type Carrier {
+    network_code: String!
+    name: String!
+    country: String!
+    network_type: String!
+  }
+
+  type OriginalCarrier {
+    network_code: String!
+    name: String!
+    country: String!
+    network_type: String!
+  }
+
+  type Roaming {
+    status: String!
+    roaming_country_code: String
+    roaming_network_code: Int
+    roaming_network_name: String
+  }
+
+  type Caller {
+    caller_type: String
+    caller_name: String
+    first_name: String
+    last_name: String
+  }
+
+  type Ip {
+    address: String
+    ip_match_level: String
+    ip_country: String
+    ip_city: String
+  }
+
+  type Query {
+    "Get details about a number"
+    insight(number: String!, type: String, cnam: Boolean, ip: String): Insight
+  }
+`;
+
+const resolvers = {
+  Query: {
+    insight: async (_source, { number, type, cnam, ip }, { dataSources }) => {
+      return dataSources.numberInsight.getInsight(number, type, cnam, ip);
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      numberInsight: new NumberInsightAPI()
+    };
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
---
+++ src/number-insight.js
@@ -0,0 +1,26 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+const config = require('config');
+
+class NumberInsightAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.nexmo.com/ni/';
+  }
+
+  willSendRequest(request) {
+    request.params.set('api_key', config.get('nexmo_api_key'));
+    request.params.set('api_secret', config.get('nexmo_api_secret'));
+  }
+
+  async getInsight(number, type = 'basic', cnam = false, ip) {
+    if (type === 'standard') {
+      return this.get(`${type}/json?number=${number}&cnam=${cnam}`);
+    } else if (type === 'advanced') {
+      return this.get(`${type}/json?number=${number}&cnam=${cnam}&ip=${ip}`);
+    } else {
+      return this.get(`${type}/json?number=${number}`);
+    }
+  }
+}
+
+module.exports = NumberInsightAPI;
