fc352644365d2fb0724f6702ee68eb5017671bed
Martyn Davies <martynrdavies@gmail.com>
2019-05-16 11:11:32 +0200

    add config flags for introspection and playground

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
 const NumberInsightAPI = require('./src/number-insight');
+const config = require('config');
 
 const typeDefs = require('./src/number-insight-types');
 
@@ -14,6 +15,10 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection:
+    process.env.GRAPHQL_INTROSPECTION || config.get('graphql_introspection'),
+  playground:
+    process.env.GRAPHQL_PLAYGROUND || config.get('graphql_playground'),
   dataSources: () => {
     return {
       numberInsight: new NumberInsightAPI()
